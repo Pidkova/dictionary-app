994436f9c0153c8d58800e20bcf67ec036adfccd
Pidkova <parmelion12@gmail.com>
2022-09-26 17:41:26 +0200

    United two components in one

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,20 +1,19 @@
 import React from "react";
-import Audio from "./Audio";
-import Transcription from "./Transcription";
+
 import "./Phonetics.css";
 
 export default function Phonetics(props) {
-  console.log(props.phonetic);
-  let audio = props.phonetic.audio;
-  let transcription = props.phonetic.text;
   return (
-    <div className="Phonetics row">
-      <div className="col audio">
-        <Audio audio={audio} />
-      </div>
-      <div className="col transcription align-content-start">
-        <Transcription transcription={transcription} />
-      </div>
+    <div className="Phonetics">
+      <a
+        href={props.phonetic.audio}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="listen-link"
+      >
+        <i className="fa-solid fa-volume-high d-inline me-2"></i> Listen
+      </a>
+      <span className="transcription">{props.phonetic.text}</span>
     </div>
   );
 }
